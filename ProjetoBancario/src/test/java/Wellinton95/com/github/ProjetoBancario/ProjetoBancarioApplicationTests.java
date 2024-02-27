2494e983f0419d612110f26d857c0f3c1ac737f3
Wellinton Costa <wellintoncoiote95@gmail.com>
2024-02-26 21:15:56 -0300

    First Commit

---
+++ ProjetoBancario/src/test/java/Wellinton95/com/github/ProjetoBancario/ProjetoBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package Wellinton95.com.github.ProjetoBancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
